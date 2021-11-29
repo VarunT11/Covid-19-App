75e2e658cb071503ab9bb3f2880f496da7071ba5
Varun Tiwari <varun.2011t@gmail.com>
2021-11-29 21:15:42 +0530

    Removed the existing files

---
+++ app/src/main/java/com/example/covid19_safetyapp/activities/DashboardActivity.java
@@ -0,0 +1,16 @@
+package com.example.covid19_safetyapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.covid19_safetyapp.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
